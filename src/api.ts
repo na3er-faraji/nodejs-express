697b9987552020d7591c9d52e8454f293afc0120
nasser faraji <faraji@bictory.io>
2023-05-28 15:03:24 +0330

    add helmet and handele error 500

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ import compression from 'compression';
 import CONFIG from "./config/environment";
 import cors from "cors";
 import bodyParser from "body-parser";
+import helmet from 'helmet';
 import swaggerUi from "swagger-ui-express";
 import { connectToDatabase } from "./models";
 import logger from './config/winston';
@@ -15,6 +16,8 @@ app.use(compression())
 app.use(cors({ origin: CONFIG.crosOrigin }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(helmet())
+app.disable('x-powered-by')
 
 // Routes
 app.use("/api", require("./routes/favorite.route"));
@@ -22,6 +25,12 @@ app.use("/api", require("./routes/profile.route"));
 app.use("/api", require("./routes/simulator.route"));
 
 app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// custom error handler
+app.use((err, req, res, next) => {
+  logger.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 app.listen(CONFIG.port, () =>
   logger.info(`✅Ready on port http://localhost:${CONFIG.port}`)
